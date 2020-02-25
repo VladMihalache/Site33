77d9a069f14fd3bcd17eb159db35d99dd110e807
VladMihalache <59883066+VladMihalache@users.noreply.github.com>
2020-02-25 20:18:24 +0200

    Slider + Accordion

--- js/main.js
+++ js/main.js
@@ -111,5 +111,118 @@ for (box of yboxes) {
     lightUp(box)
   } //?
 
-/*----------------  aniamte.js  ----------------*/
+/*----------------  slider  ----------------*/
+const slides=document.querySelectorAll(".item")
 
+let current=0;
+let prev=4;
+let next=1;
+
+function goToNum(number){
+    current=number;
+    prev=current-1;
+    next=current+1;
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("current");
+        slides[i].classList.remove("prev");
+        slides[i].classList.remove("next");
+    }
+
+    if (next == 5) {
+        next = 0;
+    }
+
+    if (prev == -1) {
+        prev = 4;
+    }
+
+    slides[current].classList.add("current")
+    slides[prev].classList.add("prev")
+    slides[next].classList.add("next")
+
+}
+
+function buttonWork(){
+const buttons=document.querySelectorAll(".buttonX");
+
+    buttons[0].addEventListener("click", ()=>{
+        if(current>0){
+            goToNum(current-1)
+        }else{
+            goToNum(4)
+        }
+    })
+    buttons[1].addEventListener("click",()=>{
+        if(current<4){
+            goToNum(current+1)
+        }else{
+            goToNum(0)
+        }
+    })
+}
+
+buttonWork()
+
+/*----------------  helpButton  ----------------*/
+
+/*const accButton1=document.querySelector(".accButton")
+const accContent1=document.querySelector(".accContent")
+
+const plus=document.querySelector(".plus")
+const minus=document.querySelector(".minus")
+
+function help(){
+    accButton1.addEventListener("click", ()=>{
+    if(accContent1.style.maxHeight){
+        accContent1.style.maxHeight=null;
+    }else{
+        accContent1.style.maxHeight=accContent1.scrollHeight + "px";
+    }
+  })  
+}
+
+help();
+*/
+/*----------------  accordion  ----------------*/
+
+/*const accordion1=document.querySelector(".accordionButton1");
+const accordion2=document.querySelector(".accordionButton2");
+const accordion3=document.querySelector(".accordionButton3");
+
+for(i=0;i<accordion.length;i++){
+    accordion[i].addEventListener("click", () => {
+        this.style.backgroundColor="#008AFF";
+        this.style.color="#D8D8DE"
+    })
+}
+
+function accordionDrop(acc){
+    acc.addEventListener("click", () => {
+        dropdownContent1.classList.toggle("visible")
+        dropdownContent2.classList.toggle("visible")
+    })
+}
+
+function open(acc){
+    acc.classList.toggle("is-opened")
+}
+
+accordionDrop(accordion1)
+accordionDrop(accordion2)
+accordionDrop(accordion3)
+*/
+const accordions = document.querySelectorAll(".accordionButton")
+
+for(let i = 0 ; i < accordions.length ; i++){
+    accordions[i].onclick = function () {
+        this.classList.toggle('is-open')
+        const content=this.nextElementSibling;
+
+        if(content.style.maxHeight){
+            content.style.maxHeight=null;
+        }else{
+            content.style.maxHeight=content.scrollHeight + "px";
+        }
+    }
+}
